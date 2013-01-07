980940997b4ac52d47bd8a1beccd1b8c60390c88
Leon Rowland <leon@rowland.nl>
2013-01-07 11:00:01 +0100

    Fixed spacing

--- views/admin/display_feeds_messages.php
+++ views/admin/display_feeds_messages.php
@@ -86,30 +86,30 @@
 		</tr>
 	</thead>
 	<tbody>
-		<?php if( !empty($feeds) && $feeds->have_posts()) : ?>
-			<?php foreach($feeds->posts as $feed): ?>
-			<?php $feed_url = get_post_meta( $feed->ID, 'pronamic_feed_url', true ); ?>
-			<?php $rss = fetch_feed( $feed_url );?>
+		<?php if ( ! empty( $feeds ) && $feeds->have_posts() ) : ?>
+			<?php foreach ( $feeds->posts as $feed ): ?>
+            <?php $feed_url     = get_post_meta( $feed->ID, 'pronamic_feed_url', true ); ?>
+            <?php $rss          = fetch_feed( $feed_url );?>
 			<tr>
 				<td><a href="<?php echo admin_url( "post.php?post={$feed->ID}&action=edit" );?>" title="<?php echo $feed_url; ?>"><?php echo $feed->post_title;?></td>
 				<td>
-					<?php if( !is_wp_error( $rss ) ): ?>
-						<?php $total = (get_option( 'pronamic_feeds_posts_per_feed' ) ?: 0 ) ;?>
+					<?php if ( ! is_wp_error( $rss ) ): ?>
+						<?php $total = ( get_option( 'pronamic_feeds_posts_per_feed' ) ?: 0 ) ;?>
                         <?php $total_messages   = $rss->get_item_quantity( $total ); ?>
                         <?php $messages         = $rss->get_items( 0, $total_messages ); ?>
-						<?php if( !empty( $messages ) ): ?>
-							<table style="width:100%">								
-								<?php foreach( $messages as $key => $message): ?>
-								<tr>
-									<td style="width:90%"><?php echo $message->get_title(); ?></td>
-									<?php if( !in_array( $message->get_id( true ), $existing_ids ) ): ?>
-										<td><a href="#" class='jAddMessage' data-id="<?php echo $key;?>" data-url="<?php echo $feed_url; ?>" data-hashedid="<?php echo $message->get_id( true ); ?>"><?php echo __( 'Add', 'pronamic_feeds' ); ?></a>
-									<?php endif;?>
-								<tr>
+						<?php if ( ! empty( $messages ) ): ?>
+							<table style="width:100%">
+								<?php foreach ( $messages as $key => $message ): ?>
+									<tr>
+										<td style="width:90%"><?php echo $message->get_title(); ?></td>
+										<?php if ( ! in_array( $message->get_id( true ), $existing_ids ) ): ?>
+											<td><a href="#" class='jAddMessage' data-id="<?php echo $key;?>" data-url="<?php echo $feed_url; ?>" data-hashedid="<?php echo $message->get_id( true ); ?>"><?php echo __( 'Add', 'pronamic_feeds' ); ?></a>
+										<?php endif;?>
+									</tr>
 								<?php endforeach;?>
 							</table>
 						<?php endif;?>
-					<?php endif;?>					
+					<?php endif;?>
 				</td>
 			</tr>
 			<?php endforeach;?>
