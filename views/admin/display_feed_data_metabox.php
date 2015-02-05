3739e6702bedb0f4d6dbd60b59fdcf1ab876645e
Remco Tolsma <info@remcotolsma.nl>
2015-02-05 10:56:56 +0100

    WordPress Coding Standards optimizations.

--- views/admin/display_feed_data_metabox.php
+++ views/admin/display_feed_data_metabox.php
@@ -1,1 +1,1 @@
-<a href="<?php echo $post_url;?>" target="_blank"><?php _e( 'From RSS Link', 'pronamic-feeds' );?>
+<a href="<?php echo esc_attr( $post_url );?>" target="_blank"><?php _e( 'From RSS Link', 'pronamic-feeds' ); ?></a>
