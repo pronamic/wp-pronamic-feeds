888407e93a9175655d1b34b50ecf2429457b4e7c
Remco Tolsma <info@remcotolsma.nl>
2015-02-05 10:07:21 +0100

    WordPress Coding Standards optimizations.

--- functions.php
+++ functions.php
@@ -1,26 +1,26 @@
 <?php
 
-function has_pronamic_feed( $id = null )
-{
-	if ( ! $id )
+function has_pronamic_feed( $id = null ) {
+	if ( ! $id ) {
 		$id = get_the_ID();
+	}
 
 	return (boolean) get_pronamic_feed( $id );
 
 }
 
-function get_pronamic_feed( $id = null )
-{
-	if ( ! $id )
+function get_pronamic_feed( $id = null ) {
+	if ( ! $id ) {
 		$id = get_the_ID();
+	}
 
 	return get_post_meta( $id, '_pronamic_feed_url', true );
 }
 
-function get_pronamic_feed_post( $id = null )
-{
-	if ( ! $id )
+function get_pronamic_feed_post( $id = null ) {
+	if ( ! $id ) {
 		$id = get_the_ID();
+	}
 
 	return get_post_meta( $id, '_pronamic_feed_post_url', true );
 }
